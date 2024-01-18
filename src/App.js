3a2a7bf778a26b8e53ab7292c869a0a04294a8fb
Langchun Zhou <zhoulangchun@gmail.com>
2024-01-18 13:22:16 -0500

    Added dividers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import me from './images/me.jpg';
 import cLogo from './images/c-logo.png';
 import cppLogo from './images/cpp-logo.png';
+import javaLogo from './images/java-logo.png';
+import pythonLogo from './images/python-logo.png';
+import htmlLogo from './images/html-logo.png';
+import cssLogo from './images/css-logo.png';
+import javascriptLogo from './images/javascript-logo.png';
+import sqlLogo from './images/sql-logo.png';
+import reactLogo from './images/react-logo.png';
 
 function App() {
 	return (
@@ -18,12 +25,25 @@ function App() {
 					<img id='me-img' src={me} />
 					<div id='hi-div'>Hi, I'm Lenny.</div>
 				</div>
-				<div id='skills-div'>
-					<h1>Skills</h1>
-					<Skill name='C' image={cLogo} />
-					<Skill name='C++' image={cppLogo} />
-				</div>
-			</main>
+				<h1>Skills</h1>
+				<h2>Languages</h2>
+				{/* <div className='skills-div'> */}
+				{/* <div className='skills-subdiv'> */}
+				<Skill name='C' image={cLogo} />
+				<Skill name='C++' image={cppLogo} />
+				<Skill name='Java' image={javaLogo} />
+				<Skill name='Python' image={pythonLogo} />
+				<Skill name='HTML' image={htmlLogo} />
+				<Skill name='CSS' image={cssLogo} />
+				<Skill name='JavaScript' image={javascriptLogo} />
+				<Skill name='SQL' image={sqlLogo} />
+				{/* </div> */}
+				{/* </div> */}
+				<hr />
+				<h2>Libraries & Frameworks</h2>
+				<Skill name='React' image={reactLogo} />
+				{/* </div> */}
+			</main >
 		</>
 	);
 }
